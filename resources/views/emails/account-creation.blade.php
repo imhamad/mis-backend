29b4befbca9bc3d99c4fc9f528cd560b5438230c
Hamad <hamad.coder@gmail.com>
2023-10-11 10:39:23 +0500

    Updates Performed:: savepoint ~ Hamad MBP

--- resources/views/emails/account-creation.blade.php
+++ resources/views/emails/account-creation.blade.php
@@ -7,7 +7,8 @@
 <b>Greetings</b> 👋<b>,</b><br><br>
 <p>We're delighted that you've signed up to be a contributor on our platform. Your account is now pending and will be shortly reviewed by our administrator to ensure it meets our basic requirements. </p>
 <p>Once your account is approved, you'll be able to access the Contributors Dashboard and start sharing your insights and expertise. We appreciate your patience during this process as we work to maintain the quality of our platform ✔️</p>
-<p>If you have any questions or need assistance, please don't hesitate to reach out to our support team at help.contributor@deknows.com. We're here to help you get started and answer any inquiries you may have.</p>
+<p>If you have any questions or need assistance, please don't hesitate to reach out to our support team at <a
+        href="mailto:help.contributor@deknows.com" style="color:black; text-decoration: underline;">help.contributor@deknows.com</a>. We're here to help you get started and answer any inquiries you may have.</p>
 <b>Best Regards,</b><br><br>
 <b>DE Powered by DeKnows Inc.</b><br><br>
 <a href="https://deknows.com"><b>https://deknows.com</b></a>
